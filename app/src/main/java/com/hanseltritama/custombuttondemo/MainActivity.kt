8d01e7f2b23c1e78fc4f8c26d7e41bb8b69c4a73
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-15 01:54:08 -0700

    added switch and custom button logic

--- app/src/main/java/com/hanseltritama/custombuttondemo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/custombuttondemo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.hanseltritama.custombuttondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        custom_button.setOnClickListener {
+            Snackbar.make(it, "Click", Snackbar.LENGTH_SHORT).show()
+        }
+        
+        switch_enable_button.setOnCheckedChangeListener { switchButton, isChecked ->
+            custom_button.isEnabled = isChecked
+        }
     }
 }
